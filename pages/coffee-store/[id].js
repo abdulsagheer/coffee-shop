a570042828551f00c720d087b4eeceb2268d31d6
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-09-22 23:20:25 +0530

    Added dynamic routes

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const CoffeeStore = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>CoffeeStore {router.query.id}</h1>
+      <button>
+        <Link href="/">Back to Home</Link>
+      </button>
+    </div>
+  );
+};
+
+export default CoffeeStore;
