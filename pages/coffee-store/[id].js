dca44392a14ad6ef86c95f295342b9a42807aa62
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-02 06:57:27 +0530

    Added get static path function to single product data fetching

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,5 +1,23 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import coffeeStoresData from "./../../data/coffee-stores.json";
+
+export async function getStaticProps({ params }) {
+  console.log("params", params.id);
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (coffeeStore) => coffeeStore.id === params.id
+      ),
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+  };
+}
 
 const CoffeeStore = () => {
   const router = useRouter();
